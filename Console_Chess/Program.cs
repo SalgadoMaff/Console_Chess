4167c3611fc8f435a57c15cbbbeeeda37baeb6fd
Felipe <lipesmaffini@gmail.com>
2023-11-18 21:18:41 -0400

    Populate board

--- Console_Chess/Program.cs
+++ Console_Chess/Program.cs
@@ -4,17 +4,10 @@ using System.Net.NetworkInformation;
 
 try
 {
-    Board board = new Board();
+
     PosDict posdict = new PosDict();
-    Match Chess_match = new Match(Color.WHITE,board);
-    board.putPiece(new Rook(Color.BLACK, board), posdict.toPosition('D', 1));
-    board.putPiece(new King(Color.BLACK, board), posdict.toPosition('E', 1));
-    board.putPiece(new Rook(Color.BLACK, board), posdict.toPosition('D', 2));
-    board.putPiece(new Rook(Color.BLACK, board), posdict.toPosition('E', 2));
-    board.putPiece(new Rook(Color.WHITE, board), posdict.toPosition('E', 8));
-    board.putPiece(new Rook(Color.WHITE, board), posdict.toPosition('E', 7));
-    board.putPiece(new King(Color.WHITE, board), posdict.toPosition('D', 8));
-    board.putPiece(new Rook(Color.WHITE, board), posdict.toPosition('D', 7));
+    Board board = populateBoard(posdict);
+    Match Chess_match = new Match(Color.WHITE, board);
     while (!Chess_match.finished)
     {
         Console.Clear();
@@ -31,14 +24,53 @@ try
 
     }
 
-    //board.putPiece(new King(Color.WHITE, board), new Position(0, 0));
-    //board.putPiece(new Queen(Color.BLACK, board), new Position(1, 0));
-    //Console.WriteLine();
-    //Console.WriteLine(posdict.toPosition('A',1).ToString());
-}
+   }
 catch (BoardException e)
 {
 
     Console.WriteLine(e.Message);
+}
+
+Board populateBoard(PosDict posdict)
+{
+    Board board = new Board();
+
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('A', 2));
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('B', 2));
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('C', 2));
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('D', 2));
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('E', 2));
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('F', 2));
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('G', 2));
+    board.putPiece(new Pawn(Color.BLACK, board), posdict.toPosition('H', 2));
+
+    board.putPiece(new Rook(Color.BLACK, board), posdict.toPosition('A', 1));
+    board.putPiece(new Knight(Color.BLACK, board), posdict.toPosition('B', 1));
+    board.putPiece(new Bishop(Color.BLACK, board), posdict.toPosition('C', 1));
+    board.putPiece(new Queen(Color.BLACK, board), posdict.toPosition('D', 1));
+    board.putPiece(new King(Color.BLACK, board), posdict.toPosition('E', 1));
+    board.putPiece(new Bishop(Color.BLACK, board), posdict.toPosition('F', 1));
+    board.putPiece(new Knight(Color.BLACK, board), posdict.toPosition('G', 1));
+    board.putPiece(new Rook(Color.BLACK, board), posdict.toPosition('H', 1));
+
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('A', 7));
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('B', 7));
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('C', 7));
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('D', 7));
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('E', 7));
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('F', 7));
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('G', 7));
+    board.putPiece(new Pawn(Color.WHITE, board), posdict.toPosition('H', 7));
+
+    board.putPiece(new Rook(Color.WHITE, board), posdict.toPosition('A', 8));
+    board.putPiece(new Knight(Color.WHITE, board), posdict.toPosition('B', 8));
+    board.putPiece(new Bishop(Color.WHITE, board), posdict.toPosition('C', 8));
+    board.putPiece(new Queen(Color.WHITE, board), posdict.toPosition('D', 8));
+    board.putPiece(new King(Color.WHITE, board), posdict.toPosition('E', 8));
+    board.putPiece(new Bishop(Color.WHITE, board), posdict.toPosition('F', 8));
+    board.putPiece(new Knight(Color.WHITE, board), posdict.toPosition('G', 8));
+    board.putPiece(new Rook(Color.WHITE, board), posdict.toPosition('H', 8));
+
+    return board;
 }
 
